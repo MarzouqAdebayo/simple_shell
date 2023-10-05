d30dba71be0c6267e9f3b46b616cffa8afc0300d
AdebayoMarzouq <marzouqdevmail@gmail.com>
2023-10-05 12:39:53 +0100

    Initial commit

---
+++ hsh.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - contains the main function of the program
+ * @argc: Arguments passed into the CLI
+ * @argv: Pointer to arguments string
+ *
+ * Description: A simple UNIX shell
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+	/** Read */
+	/** Parse */
+	/** Execute */
+	return (0);
+}
