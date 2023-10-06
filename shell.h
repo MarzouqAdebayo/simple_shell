d0ae3ad18519e17d7594a9ed6b44b5648b4c1e26
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2023-10-06 14:46:59 +0100

    Fixed light bugs yet again

--- shell.h
+++ shell.h
@@ -12,6 +12,7 @@ char **_parse(char *str);
 /** String util functions */
 int iswhitespace(char s);
 char *collapse_whitespace(char *old, char *new);
+char **split_str(char *src, const char delimiter);
 size_t _strlen(const char *str);
 char *_strncpy(char *dest, const char *src, size_t n);
 
