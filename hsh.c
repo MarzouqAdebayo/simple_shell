bb7958ae5fecab99c39c6db380ca9f8fd48ff90d
AdebayoMarzouq <marzouqdevmail@gmail.com>
2023-10-06 09:19:45 +0100

    Prompt test

--- hsh.c
+++ hsh.c
@@ -11,7 +11,22 @@
 
 int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t bytes_read = 0;
 	/** Read */
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			prompt(NULL);
+		bytes_read = getline(&line, &len, stdin);
+		if (bytes_read == -1)
+		{
+			free(line);
+			exit(1);
+		}
+		printf("%s\n", line);
+	}
 	/** Parse */
 	/** Execute */
 	return (0);
--- shell.h
+++ shell.h
@@ -1,5 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void prompt(char *sym);
 
 #endif
