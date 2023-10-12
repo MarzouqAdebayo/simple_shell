cfa1fe78dd507765bc93da17f7e6fddf30dc6f7d
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2023-10-12 15:08:59 +0100

    Implemented atoi function

--- string_utils.c
+++ string_utils.c
@@ -87,3 +87,33 @@ char *_strtok(char *str, const char *delim)
 	}
 	return (NULL);
 }
+
+/**
+ * _atoi - Body of the function
+ * @s: The character to be parsed
+ *
+ * Description: converts strings into int
+ * Return: has no return value
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, result = 0, sign = 1;
+
+	while (*(s + i) < '0' || *(s + i) > '9')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+
+	while (*(s + i) >= '0' && *(s + i) <= '9')
+	{
+		result = (result * 10) + (s[i] - 48);
+		i++;
+	}
+
+	return (result * sign);
+}
